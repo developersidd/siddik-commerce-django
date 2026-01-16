8da5e51bb83228bb3e5fc32b5e9284b93dee028e
Emon8794 <emonabujafor@gmail.com>
2026-01-17 00:44:17 +0600

    fix: Set DEBUG to False for production environment

--- alistyle/settings.py
+++ alistyle/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
 
-DEBUG = config("DEBUG", cast=bool, default=True)
+DEBUG = config("DEBUG", cast=bool, default=False)
 
 ALLOWED_HOSTS = ["siddik.pythonanywhere.com"]
 
